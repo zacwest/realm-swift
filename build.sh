c94ece654ea03ef3a387921a7e77478e87110853
Jason Flax <jsflax@gmail.com>
2020-09-16 19:37:18 +0100

    add more complete evg file

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@
 #          macOS uses a POSIX complain version of bash as /bin/sh, but apparently it does
 #          not strip away this feature. Also, this will fail if somebody forces the script
 #          to be run with zsh.
-set -o pipefail
+set -o verbose
 set -e
 
 source_root="$(dirname "$0")"
