2a4e2200a11d4af7cadb51d1a9d4dba092d28f7c
Thomas Goyne <tg@realm.io>
2020-09-17 12:38:39 -0700

    minimal repro

---
+++ Sources/RealmCocoa/RealmCocoa.mm
@@ -0,0 +1,5 @@
+#define REALM_NO_CONFIG
+#import "RealmCocoa.h"
+#import <realm/util/features.h>
+
+int RLMFoo() { return 0; }
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import RealmCocoaTests
+
+var tests = [XCTestCaseEntry]()
+tests += RealmCocoaTests.allTests()
+XCTMain(tests)
