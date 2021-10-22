28d8ee6475b290ec85067bf4fbb70f7d4b52083b
Lee Maguire <lee.maguire@mongodb.com>
2021-10-22 22:09:05 +0100

    Use sync-progress core branch

--- Package.swift
+++ Package.swift
@@ -122,7 +122,7 @@ let package = Package(
             targets: ["Realm", "RealmSwift"]),
     ],
     dependencies: [
-        .package(name: "RealmDatabase", url: "https://github.com/realm/realm-core", .exact(Version(coreVersionStr)!))
+        .package(name: "RealmDatabase", url: "https://github.com/realm/realm-core", .branch("lm/sync-progress"))
     ],
     targets: [
       .target(
