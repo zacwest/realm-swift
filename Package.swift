ebe5e8e3ece6c4e62dcf0a7fc304f533da51255a
Lee Maguire <lee.maguire@mongodb.com>
2021-10-28 09:55:36 +0100

    change core branch

--- Package.swift
+++ Package.swift
@@ -122,7 +122,7 @@ let package = Package(
             targets: ["Realm", "RealmSwift"]),
     ],
     dependencies: [
-        .package(name: "RealmDatabase", url: "https://github.com/realm/realm-core", .branch("lm/sync-progress"))
+        .package(name: "RealmDatabase", url: "https://github.com/realm/realm-core", .branch("lm/sync-test"))
     ],
     targets: [
       .target(
